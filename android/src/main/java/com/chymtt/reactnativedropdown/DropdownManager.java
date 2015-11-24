d79f6fffbaaa04e2741f00cd6513ad7e61c31181
Brian DeWeese <brian@briandeweese.org>
2015-11-24 11:42:28 -0800

    attempting react 0.15 support
    Signed-off-by: Brian DeWeese <brian@briandeweese.org>

--- android/src/main/java/com/chymtt/reactnativedropdown/DropdownManager.java
+++ android/src/main/java/com/chymtt/reactnativedropdown/DropdownManager.java
@@ -24,16 +24,26 @@ public class DropdownManager extends SimpleViewManager<Dropdown> {
     @UIProp(UIProp.Type.NUMBER)
     public static final String PROP_SELECTED = "selected";
 
-    @Override
-    public void updateView(Dropdown view, CatalystStylesDiffMap props) {
-        super.updateView(view, props);
-
-        if (props.hasKey(PROP_VALUES)) {
-            view.setValues(props.getArray(PROP_VALUES));
-        }
-
-        if (props.hasKey(PROP_SELECTED)) {
-            view.setSelected(props.getInt(PROP_SELECTED, 0));
-        }
+    @ReactProp(name = PROP_VALUES)
+    public void setSrc(Dropdown view, @Nullable CatalystStylesDiffMap props) {
+    view.setValues(props.getArray(PROP_VALUES));
     }
+
+    @ReactProp(name = PROP_SELECTED)
+    public void setSrc(Dropdown view, @Nullable CatalystStylesDiffMap props) {
+    view.setSelected(props.getInt(PROP_SELECTED, 0));
+    }
+
+    // @Override
+    // public void updateView(Dropdown view, CatalystStylesDiffMap props) {
+    //     super.updateView(view, props);
+    //
+    //     if (props.hasKey(PROP_VALUES)) {
+    //         view.setValues(props.getArray(PROP_VALUES));
+    //     }
+    //
+    //     if (props.hasKey(PROP_SELECTED)) {
+    //         view.setSelected(props.getInt(PROP_SELECTED, 0));
+    //     }
+    // }
 }
